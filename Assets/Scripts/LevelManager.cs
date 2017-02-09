527fa268d42b928c1889fece313654e4c3b53261
Joshua Felker <jafelker@umich.edu>
2017-02-09 03:53:06 -0500

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    void Awake() {
+        //if(Application.loadedLevelName == "Win Screen") {
+        //	LifeManager.livesLeft = 0;
+        //}
+        if(SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByName("Win Screen"))) {
+            LifeManager.livesLeft = 0;
+        } 
+    }
+
+    public void LoadLevel(string sceneName){
+        Debug.Log ("New Level load: " + sceneName);
+        Brick.breakableCount = 0;
+        //Application.LoadLevel (name);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitRequest(){
+        Debug.Log ("Quit requested");
+        Application.Quit ();
+    }
+    public void LoadNextLevel() {
+        Brick.breakableCount = 0;
+//		Application.LoadLevel(Application.loadedLevel + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void BrickDestroyed() {
+        if(Brick.breakableCount <= 0) {
+            LoadNextLevel();
+        }
+    }
+
+    public void DroppedBall() {
+        LifeManager.livesLeft--;
+        Debug.Log(LifeManager.livesLeft);
+        if(LifeManager.livesLeft <=0) {
+            LoadLevel ("Lose Screen");
+        } else {
+            Ball.hasStarted = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	public bool autoPlay = false;
+
+	private Ball ball;
+
+	// Use this for initialization
+	void Start () {
+	    this.ball = GameObject.FindObjectOfType<Ball>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!this.autoPlay) {
+			MoveWithMouse ();
+		} else {
+			AutoPlay();
+		}
+	}
+	void MoveWithMouse() {
+	    Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		float mousePosInBlocks = Input.mousePosition.x/Screen.width*16 - 0.5f;
+		Vector3 paddlePos = new Vector3 (Mathf.Clamp(worldPos.x -0.5f, 0f, 15f), this.transform.position.y, -1f);
+		this.transform.position = paddlePos;
+	}
+
+	void AutoPlay() {
+		Vector3 paddlePos = new Vector3(Mathf.Clamp(this.ball.transform.position.x-0.5f,0f,15f),this.transform.position.y,-1f);
+		this.transform.position = paddlePos;
+	}
+}
